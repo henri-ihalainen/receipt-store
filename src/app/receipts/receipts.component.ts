6925d442c9a37d8689b48a2c076c27a6d2b7cfbe
henri <henri.ihalainen@gofore.com>
2016-11-03 20:42:27 +0200

    Add and list receipts

--- src/app/receipts/receipts.component.ts
+++ src/app/receipts/receipts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFire } from 'angularfire2';
+import { AngularFire, FirebaseAuthState } from 'angularfire2';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,15 +8,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./receipts.component.css']
 })
 export class ReceiptsComponent implements OnInit {
+  private displayName;
+  private receipts;
 
   constructor(private af: AngularFire, private router: Router) {
   }
 
   ngOnInit() {
+    this.af.auth.subscribe((state: FirebaseAuthState) => {
+      if (state) {
+        this.displayName = state.auth.displayName
+      }
+    });
+
+    this.receipts = this.af.database.list('/receipts');
+  }
+
+  add(name, amount) {
+    this.receipts.push({name: name, amount: amount, user: this.displayName});
   }
 
   logout() {
     this.af.auth.logout();
-    this.router.navigate(['login'])
+    this.router.navigate(['login']);
   }
 }
